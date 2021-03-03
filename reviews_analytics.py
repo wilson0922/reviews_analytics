3b259cb07dc0ca228050f994d889e79016676ec2
wilson0922 <tree70373@gmail.com>
2021-03-03 18:09:15 +0800

    avg_len

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -7,3 +7,8 @@ with open('reviews.txt', 'r') as f:
 		if count % 10000 == 0:
 			print(len(data))
 print('檔案讀取完了，總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('平均留言長度為:', sum_len/len(data),'字')
